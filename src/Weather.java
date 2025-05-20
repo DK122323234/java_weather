85958ebe9df9df04fd5303b4222e14e174372ee9
DK122323234 <dkamensky@licei10.ru>
2025-05-20 15:58:15 +0300

    добавил более понятные названия

--- src/Weather.java
+++ src/Weather.java
@@ -1,4 +1,3 @@
-
 import netscape.javascript.JSObject;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,49 +12,46 @@ import java.net.URL;
 import java.util.Scanner;
 
 public class Weather {
-    private static String ip = "29ed5ca47c3d0c32385b18f1e82e522f";
-    public static String city;
+    private static String apiKey = "29ed5ca47c3d0c32385b18f1e82e522f";
+    public static String cityName;
 
     public static void main(String[] args) throws IOException {
 
         try {
             // URL для получения информации о местоположении по IP
-            String url = "https://ipinfo.io/city";
-            BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
+            String locationUrl = "https://ipinfo.io/city";
+            BufferedReader locationReader = new BufferedReader(new InputStreamReader(new URL(locationUrl).openStream()));
+            StringBuilder locationResponse = new StringBuilder();
+            String locationLine;
+            while ((locationLine = locationReader.readLine()) != null) {
+                locationResponse.append(locationLine);
             }
-            city = sb.toString();
+            cityName = locationResponse.toString();
         } catch (Exception e) {
             System.out.println(e.toString());
         }
 
-
         try {
-            String ur = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + ip + "&units=metric";
-            BufferedReader br = new BufferedReader(new InputStreamReader(new URL(ur).openStream()));
+            String weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=metric";
+            BufferedReader weatherReader = new BufferedReader(new InputStreamReader(new URL(weatherUrl).openStream()));
 
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
+            StringBuilder weatherResponse = new StringBuilder();
+            String weatherLine;
+            while ((weatherLine = weatherReader.readLine()) != null) {
+                weatherResponse.append(weatherLine);
             }
 
             for (int i = 0; i < 7; i++) {
-
-                JSONObject jsonObject = new JSONObject(sb.toString());
-                JSONArray jsonArray = jsonObject.getJSONArray("list");
-                JSONObject mai = jsonArray.getJSONObject(i);
-                JSONObject main = mai.getJSONObject("main");
-                double temperature = main.getDouble("temp");
-                System.out.println("Температура:" + temperature);
+                JSONObject jsonObject = new JSONObject(weatherResponse.toString());
+                JSONArray weatherArray = jsonObject.getJSONArray("list");
+                JSONObject forecast = weatherArray.getJSONObject(i);
+                JSONObject mainData = forecast.getJSONObject("main");
+                double temperature = mainData.getDouble("temp");
+                System.out.println("Температура: " + temperature);
             }
 
-
         } catch (Exception e) {
-            System.out.println(e.toString());
+            System.out.println("Что-то пошло не так");
             e.printStackTrace();
         }
     }
